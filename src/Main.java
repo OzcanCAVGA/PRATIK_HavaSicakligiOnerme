bf5e2ea100688734ca9a567c4aa05aa27501457f
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:08:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+/*
+Koşullar :
+
+Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+Sıcaklık 10 ve 25 arasında ise "Piknik" etkinliğini öner.
+Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+Ödev
+Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi
+farklı çözüm yolları bulunuz.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sicaklik;
+        Scanner yazici = new Scanner(System.in);
+        System.out.print("Lutfen sicaklik degerini giriniz: ");
+        sicaklik = yazici.nextInt();
+        if (5 > sicaklik)
+            System.out.println("Kayak yapabilirsiniz.");
+        else if (sicaklik < 25) {
+            if (sicaklik<15)
+                System.out.println("Sinemaya Gidebilirsiniz");
+            if (sicaklik>=10 && sicaklik<25)
+                System.out.println("Piknige gidebilirsiniz.");
+
+        }
+        else
+            System.out.println("Yuzmeye gidebilirsiniz.");
+
     }
 }
